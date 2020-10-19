b48edf0ad3bd4f82c278ae7461efb79180d49975
XuanThanhTb <xuanthanh180599@gmail.com>
2020-10-20 01:26:19 +0700

    thanh fix xong image

---
+++ app/src/main/java/com/letsbuildthatapp/kotlinmessenger/messages/ShowMenuMessActivity.kt
@@ -0,0 +1,11 @@
+package com.letsbuildthatapp.kotlinmessenger.messages
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowMenuMessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_menu_mess)
+    }
+}
